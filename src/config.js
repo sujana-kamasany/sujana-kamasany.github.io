666172106ddc5298db4bd1e6040211b809bd8036
Sujana Kamasany <67040886+sujana-kamasany@users.noreply.github.com>
2022-02-05 08:19:25 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,93 +1,67 @@
 // config.js
 module.exports = {
     github: {
-        username: 'arifszn', // Your GitHub org/user name. (Required)
+        username: 'sujana-kamasany', // Your GitHub org/user name. (Required)
         sortBy: 'stars', // stars | updated
-        limit: 8, // How many projects to display.
+        limit: 4, // How many projects to display.
         exclude: {
             forks: false, // Forked projects will not be displayed if set to true.
-            projects: ['laravel-ecommerce'] // These projects will not be displayed. example: ['my-project1', 'my-project2']
+            projects: ['java'] // These projects will not be displayed. example: ['my-project1', 'my-project2']
         }
     },
     social: {
-        linkedin: 'ariful-alam',
-        twitter: 'arif_swozon',
+        linkedin: 'sujana--k',
+        twitter: 'sujana_kamasany',
         facebook: '',
         dribbble: '',
         behance: '',
         medium: '',
         devto: 'arifszn',
-        website: 'https://arifszn.github.io',
+        website: 'https://sujana-kamasany.github.io',
         phone: '',
-        email: 'arifulalamszn@gmail.com'
+        email: 'sujanakamasany@gmail.com'
     },
     skills: [
-        'PHP',
-        'Laravel',
-        'JavaScript',
-        'React.js',
-        'Node.js',
+        'Python',
+        'HTML',
+        'CSS',
+        'C Programming '
         'MySQL',
         'Git',
-        'Docker',
-        'CSS',
-        'Antd',
-        'Tailwind',
-        'Bootstrap',
     ],
     experiences: [
         { 
-            company: 'Monstarlab Bangladesh',
-            position: 'Backend Engineer II',
+            company: 'THE SPARKS FOUNDATION ',
+            position: 'Web Developement Intern ',
             from: 'September 2021',
-            to: 'Present'
+            to: 'October 2021'
         },
         { 
-            company: 'Orangetoolz',
-            position: 'Jr. Full Stack Engineer',
-            from: 'July 2019',
-            to: 'August 2021'
+            company: 'Girl Script winter of code',
+            position: 'Contributor ',
+            from: 'August 2021',
+            to: 'December 2021'
         },
-        { 
-            company: 'Techvillage',
-            position: 'Jr. Software Engineer',
-            from: 'January 2019',
-            to: ' June 2019'
-        }
+      
     ],
     education: [
         { 
-            institution: 'American International University-Bangladesh',
-            degree: 'Bachelor of Science',
-            from: '2015',
-            to: '2019'
+            institution: 'Gandhi Institute of Technology and Management ',
+            degree: 'Bachelor of Technology in computer science ',
+            from: '2019',
+            to: '2023'
         },
-        { 
-            institution: 'Cantonment College, Jessore',
-            degree: 'Higher Secondary Certificate (HSC)',
-            from: '2012',
-            to: '2014',
-        },
-        { 
-            institution: 'Chowgacha Shahadat Pilot High School',
-            degree: 'Secondary School Certificate (SSC)',
-            from: '2007',
-            to: '2012'
-        }
+        
     ],
     blog: {
         // Display blog posts from your medium or dev.to account. (Optional)
         source: 'dev.to', // medium | dev.to
-        username: 'arifszn',
-        limit: 3 // How many posts to display. Max is 10.
+        username: 'sujanakamasany',
+        limit: 1 // How many posts to display. Max is 10.
     },
     googleAnalytics: {
         // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
-        id: 'G-WLLB5E14M6' // Please remove this and use your own tag id or keep it empty
-    },
-    hotjar: {
-        id: '2617601', //  Please remove this and use your own id or keep it empty
-        snippetVersion : 6
+        id: '' // Please remove this and use your own tag id or keep it empty
     },
     themeConfig: {
         default: 'light',
